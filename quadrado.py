e9d467ec1f58b273954dc8e0b266369b05a2cbe4
Rober Guerra <roberzguerra@gmail.com>
2019-07-01 20:11:34 -0300

    Classe quadrado.

--- quadrado.py
+++ quadrado.py
@@ -2,6 +2,8 @@
 
 from helper import input_type
 
+lado = 10
+
 class Quadrado:
     """
     Nomes de classes devem utilizar o  padrao CamelCase:
@@ -15,8 +17,8 @@ class Quadrado:
     nome = ''
 
     # Métodos
-    def set_lado(self, valor_lado):
-        self.lado = valor_lado
+    def set_lado(self, valor):
+        self.lado = valor
 
     def get_lado(self):
         return "%0.2f cm" % (float(self.lado))
@@ -28,16 +30,46 @@ class Quadrado:
         area = self.calcular_area()
         return "%0.2f cm²" % (area)
 
+    def __str__(self):
+        return self.nome
+
+    def imprimir(self):
+        area_superior = ' ' + ('-' * self.lado)
+        area_lateral = '|' + (' ' * self.lado) + '|'
+        
+        print(area_superior);
+        for i in range(0, self.lado):
+            print(area_lateral)
+        print(area_superior)
+
+    def teste_args(self, *args):
+        """
+        *args recebe multiplos parametros e os transforma em
+        uma tupla.
+        Ex de chamada: objeto.teste_args(1, 2, 3);
+        resultando na tupla: (1,2,3)
+        """
+        print(args)
+
+    def teste_kwargs(self, **kwargs):
+        """
+        *kwargs recebe multiplos parametros nomeados e os transforma em
+        um dicionario, contendo o nome como a chave do valor.
+        Ex: objeto.teste_kwargs(nome='rober', param1=10)
+        resultando no dicionario: {'nome': "rober", 'param1': 10}
+        """
+        print(kwargs)
+
 
 # execucaodo meu programa
 quad = Quadrado()
-
-quad.lado = input_type("Informe o lado do quadrado: ", 'float>0')
-print("A área do quadrado é %s." % (quad.area_formatada()))
-
+quad.lado = 10
+quad.set_lado(10)
 
 
-
-    
-
-
+#  ----- 
+# |     |
+# |     |
+# |     |
+# |     |
+#  ----- 
